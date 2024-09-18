974f69fa47321bb1dc7bb4d30e1ebcb44a3f850d
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-18 22:33:11 +0900

    Add the GitHub Apps OAuth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import type { Component } from 'solid-js';
+import { Show, type Component } from 'solid-js';
+import cookie from 'cookie';
 
 const App: Component = () => {
+  const tokens = cookie.parse(document.cookie);
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <>
+      <nav>
+        <button>
+          <a href={`https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_GITHUB_APP_CLIENT_ID}&redirect_uri=${window.location.origin}/api`}>Login with GitHub</a>
+        </button>
+      </nav>
+      <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+      <Show when={tokens}>
+        <p>
+          {Object.keys(tokens)}
+        </p>
+      </Show>
+    </>
   );
 };
 
