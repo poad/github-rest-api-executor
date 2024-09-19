acb1036eb882aa93ed8b984e882b0955497dd47a
Kenji Saito <1867845+poad@users.noreply.github.com>
2024-09-19 19:37:48 +0900

    Add the GitHub Apps OAuth (#1)

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,100 @@
+import cookie from 'cookie';
+
 export const config = {
-  runtime: 'edge',
+  maxDuration: 60,
 };
-   
-export default (request: Request) => {
-  return new Response(`Hello, from ${request.url} I'm now an Edge Function!`);
-};
-  
+
+interface AccessTokenResponse {
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  refresh_token_expires_in: number;
+  scope: string;
+  token_type: string;
+}
+
+async function redirectWithTokenCookies(origin: string, response: Response) {
+  const tokens = await (response.json() as Promise<AccessTokenResponse>);
+  const expires = new Date(new Date().getTime() + tokens.expires_in * 1000);
+  const refreshTokenExpires = new Date(new Date().getTime() + tokens.refresh_token_expires_in * 1000);
+
+  const cookieAccessToken = cookie.serialize(
+    'access-token', tokens.access_token, {
+      secure: true,
+      httpOnly: true,
+      path: '/',
+      expires,
+    });
+
+  const cookieRefreshToken = cookie.serialize(
+    'refresh-token', tokens.refresh_token, {
+      secure: true,
+      httpOnly: true,
+      path: '/',
+      expires: refreshTokenExpires,
+    },
+  );
+
+  return new Response(null, {
+    headers: [
+      ['Location', `${origin}/`],
+      ['Set-Cookie', cookieAccessToken],
+      ['Set-Cookie', cookieRefreshToken],
+    ],
+    status: 303,
+  });
+}
+
+export async function GET(request: Request) {
+  const { GITHUB_APP_CLIENT_ID, GITHUB_APP_CLIENT_SECRET } = process.env;
+  const url = new URL(request.url);
+
+  const origin = url.origin;
+  const headers = request.headers;
+  if (headers.has('Cookie')) {
+    const cookies = headers.get('Cookie').split('; ').map((keyValue) => keyValue.split('='));
+    if (cookies.some((cookie) => cookie[0] === 'access-token')) {
+      return Response.redirect(`${origin}/`);
+    }
+    const refreshToken = cookies.find((cookie) => cookie[0] === 'refresh-token')?.[1];
+    if (refreshToken) {
+      const queryString = new URLSearchParams({
+        client_id: GITHUB_APP_CLIENT_ID,
+        client_secret: GITHUB_APP_CLIENT_SECRET,
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+      }).toString();
+      const response = await fetch(
+        `https://github.com/login/oauth/access_token?${queryString}`,
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+      );
+      return redirectWithTokenCookies(origin, response);
+    }
+  }
+  const params = url.searchParams;
+  const code = params.get('code');
+  if (!code) {
+    return Response.redirect(`${origin}/`);
+  }
+  const queryString = new URLSearchParams({
+    client_id: GITHUB_APP_CLIENT_ID,
+    client_secret: GITHUB_APP_CLIENT_SECRET,
+    code,
+  }).toString();
+  const response = await fetch(
+    `https://github.com/login/oauth/access_token?${queryString}`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    },
+  );
+
+  return redirectWithTokenCookies(origin, response);
+}
